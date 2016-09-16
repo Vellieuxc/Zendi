b9d1b6a6d5aeee133b49f03eadbe358706dcf704
Clement Vellieux <clement.vellieux@gmail.com>
2016-09-16 17:01:49 +0430

    create and new actions created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,23 @@
 class CommentsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :new, :create]
   def index
-    @comments=Comment.first
+    @comments=Comment.all
+  end
+  def new
+    @comment=Comment.new
+  end
+  def create
+    @comment=Comment.new(comment_params)
+    @comment.save
+    if @comment.save
+      redirect_to comments_path
+    # else
+      # render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :doctor_id)
   end
 end
