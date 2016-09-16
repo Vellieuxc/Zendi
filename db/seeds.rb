5434c5beee125607894bc2c592a82d5d7ce855e1
Clement Vellieux <clement.vellieux@gmail.com>
2016-09-16 11:00:49 +0430

    read action for comment added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+
+c1=Comment.create!(text: "test")
